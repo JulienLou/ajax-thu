b78945c69d8d5bc6a17a362e76c58124b88d5eb8
JulienLou <75163208+JulienLou@users.noreply.github.com>
2020-12-18 14:05:38 +0100

    formulaire add

--- js/script.js
+++ js/script.js
@@ -1,2 +1,16 @@
 // script.js
 
+$(document).ready(function(){
+    $("form").submit(function(event){
+        $("input, textarea").each(function(){
+            if($(this).val().length === 0){
+                $(this).css("border", "2px solid red");
+                $(this).after("<span>Merci de remplir ce champ.</span>");
+                event.preventDefault();
+            }else{
+                let chaine = $("form").serialize();
+            }
+        });
+        
+    });
+});
